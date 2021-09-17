018c53bd201aa7b1a5d82f617e805d52cd46eb21
alok <alok.sharma.17@mountblue.tech>
2021-09-17 12:53:33 +0530

    callback1 using promise

--- callback1.js
+++ callback1.js
@@ -1,12 +1,33 @@
 function callback1(boardId, board, cb) {
-  setTimeout(() => {
-    const data = board.find((item) => item.id === boardId);
-    if (data) {
-      cb(data);
-    } else {
-      cb(null, "Id Not Found");
-    }
-  }, 2 * 1000);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const data = board.find((item) => item.id === boardId);
+      if (data) {
+        resolve(cb(data));
+      } else {
+        reject(cb(null, "Id Not Found"));
+      }
+    }, 2 * 1000);
+  });
 }
 
 module.exports = { callback1 };
+
+// function callback1(boardId, board) {
+//   return new Promise((resolve, reject) => {
+//     const data = board.find((item) => item.id === boardId);
+//     if (data) {
+//       resolve(data);
+//     } else {
+//       reject("Id Not Found");
+//     }
+//   })
+//     .then((data) => {
+//       console.log(data);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// }
+
+// module.exports = { callback1 };
