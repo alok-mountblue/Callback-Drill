638db79b019c90c6a8f2c1f91508d8408bc01ac3
alok <alok.sharma.17@mountblue.tech>
2021-09-21 11:56:24 +0530

    Adding Questions on Problems

--- test/testCallback2.js
+++ test/testCallback2.js
@@ -1,3 +1,5 @@
+// Problem 2: Write a function that will return all lists that belong to a board based on the boardID that is passed to it from the given data in lists.json. Then pass control back to the code that called it by using a callback function.
+
 const { callback2 } = require("../callback2");
 const lists = require("../Jdata/lists.json");
 
